e63d59527765805be5eb04fe88fd0b13c3f9a323
Kyaw Naing Thu <kyawnaingthu225@gmail.com>
2021-06-06 21:11:34 +0630

    Components Within Components

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+Vue.component('task-list',{
+    template: `
+        <div>
+            <task v-for="task in tasks">{{ task.description }}</task>
+        </div>
+    `,
+
+    data() {
+        return {
+            tasks: [
+                { description: 'Go to Store', completed: true},
+                { description: 'Sleep', completed: true},
+                { description: 'Eat', completed: false},
+                { description: 'Exercise', completed: false}
+            ]
+        }
+    }
+});
+
 Vue.component('task',{
     template: '<li><slot></slot></li>'
 });
